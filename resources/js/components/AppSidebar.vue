2e341f7d2cf3d18a7bf473c219efa5643e039ba5
José Reyes Palacio <jrpalacio.dev@gmail.com>
2025-06-05 19:39:29 -0600

    init proyect

---
+++ resources/js/components/AppSidebar.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import NavFooter from '@/components/NavFooter.vue';
+import NavMain from '@/components/NavMain.vue';
+import NavUser from '@/components/NavUser.vue';
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import { type NavItem } from '@/types';
+import { Link } from '@inertiajs/vue3';
+import { BookOpen, Folder, LayoutGrid, Plus } from 'lucide-vue-next';
+import AppLogo from './AppLogo.vue';
+
+const mainNavItems: NavItem[] = [
+    {
+        title: 'Inicio',
+        href: '/dashboard',
+        icon: LayoutGrid,
+    },
+    {
+        title: 'Nuevo registro',
+        href: '/register-product',
+        icon: Plus,
+    }
+];
+
+const footerNavItems: NavItem[] = [
+    {
+        title: 'Github Repo',
+        href: 'https://github.com/laravel/vue-starter-kit',
+        icon: Folder,
+    },
+    {
+        title: 'Documentación',
+        href: 'https://laravel.com/docs/starter-kits#vue',
+        icon: BookOpen,
+    },
+];
+</script>
+
+<template>
+    <Sidebar collapsible="icon" variant="inset">
+        <SidebarHeader>
+            <SidebarMenu>
+                <SidebarMenuItem>
+                    <SidebarMenuButton size="lg" as-child>
+                        <Link :href="route('dashboard')">
+                            <AppLogo />
+                        </Link>
+                    </SidebarMenuButton>
+                </SidebarMenuItem>
+            </SidebarMenu>
+        </SidebarHeader>
+
+        <SidebarContent>
+            <NavMain :items="mainNavItems" />
+        </SidebarContent>
+
+        <SidebarFooter>
+
+            <NavUser />
+        </SidebarFooter>
+    </Sidebar>
+    <slot />
+</template>
---
+++ resources/js/pages/Dashboard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+
+const breadcrumbs: BreadcrumbItem[] = [
+  {
+      title: 'Productos',
+      href: '/dashboard',
+  },
+];
+</script>
+
+<template>
+  <Head title="Dashboard" />
+
+  <AppLayout :breadcrumbs="breadcrumbs">
+    <section class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+        <div class="grid auto-rows-min gap-4 md:grid-cols-2 lg:grid-cols-4">
+            <article class="relative overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border p-4 flex items-center justify-between">
+
+                <img src="https://picsum.photos/200" alt="Producto" class="size-20 rounded-xl" />
+                <div>
+                    <h2 class="text-xl font-semibold">Nombre del producto</h2>
+                    <p class="text-sm text-muted-foreground">Descripción del producto</p>
+                    <p class="text-xl font-semibold">1000</p>
+                </div>
+            </article>
+        </div>
+    </section>
+  </AppLayout>
+</template>
