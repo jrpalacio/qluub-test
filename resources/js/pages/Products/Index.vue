02e1bf5e6f93980feaa4b1b2f27b3514bcf89b71
José Reyes Palacio <jrpalacio.dev@gmail.com>
2025-06-07 20:35:05 -0600

    feat(web): agregar Index Products

---
+++ resources/js/pages/Products/Index.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head, Link } from '@inertiajs/vue3';
+import { defineProps } from 'vue';
+import { Box, Trash2, Pencil } from 'lucide-vue-next';
+
+const breadcrumbs: BreadcrumbItem[] = [
+	{
+			title: 'Productos',
+			href: '/products',
+	},
+];
+
+const props = defineProps({
+		products: {
+				type: Array,
+				required: true,
+		},
+});
+
+</script>
+
+<template>
+	<Head title="Productos" />
+
+	<AppLayout :breadcrumbs="breadcrumbs">
+		<section class="py-8 antialiased md:py-12">
+			<div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
+				<!-- Heading & Filters -->
+				<div class="mb-4 items-end justify-between space-y-4 sm:flex sm:space-y-0 md:mb-8">
+                    <h2 class="mt-3 text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">Productos</h2>
+                    <Link href="/products/create" class="rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700">Agregar producto</Link>
+				</div>
+				<div v-if="products.length > 0" class="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4 mt-8">
+                    <div v-for="product in products" :key="product.id" class="relative rounded-3xl border border-gray-200 bg-white p-6 shadow-sm dark:border-neutral-700 dark:bg-neutral-800">
+                        <div class="pt-6">
+                            <Link
+                            :href="route('products.destroy', product.id)"
+                            method="delete"
+                            as="button"
+                            class="absolute top-4 right-4 rounded-lg border border-red-200 bg-red-100 px-3 py-2 text-sm font-medium text-red-600 hover:bg-red-200 hover:text-red-800 focus:z-10 focus:outline-none focus:ring-4 focus:ring-red-100 dark:border-red-600 dark:bg-red-800 dark:text-red-200 dark:hover:bg-red-700 dark:hover:text-white dark:focus:ring-red-700"
+                            >
+                                <Trash2 />
+                            </Link>
+                            <div class="flex justify-between items-center">
+                                <p class="font-extrabold leading-tight text-gray-900 dark:text-white">{{ product.stock }} U.</p>
+                            </div>
+                            <div class="flex items-center gap-4 mt-6">
+                                <Box />
+                                <h3 class="text-lg font-semibold leading-tight text-gray-900 dark:text-white">{{ product.name }}</h3>
+                            </div>
+                            <p class="text-sm font-medium text-gray-500 dark:text-gray-400 mt-2">{{ product.description }}</p>
+                            <div class="flex items-center gap-4 mt-6">
+                                <Link :href="route('products.edit', product.id)" method="get" as="button" class="rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700">
+                                    <Pencil />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+				</div>
+                <div class="flex items-center justify-center mt-8" v-else>
+                    <p class="text-gray-500">No hay productos</p>
+                </div>
+			</div>
+		</section>
+	</AppLayout>
+</template>
