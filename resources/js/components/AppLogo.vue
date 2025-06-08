6fe0864195596ba51bbceacad079ba482c67608e
José Reyes Palacio <jrpalacio.dev@gmail.com>
2025-06-07 22:48:28 -0600

    feat(AppLogo): implementa logo-simple

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import AppLogoIcon from '@/components/AppLogoIcon.vue';
 </script>
 
 <template>
     <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-        <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
+        <img src="images/logo-simple.png" alt="Logo QLuub" class="w-full h-full object-contain p-1" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
         <span class="mb-0.5 truncate font-semibold leading-none">QLuub</span>
