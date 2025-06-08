6ab87572320887af24a3b557f4308c6924f07a98
José Reyes Palacio <jrpalacio.dev@gmail.com>
2025-06-07 22:30:28 -0600

    feat(AuthSimpleLayout): usar logo

--- resources/js/layouts/auth/AuthSimpleLayout.vue
+++ resources/js/layouts/auth/AuthSimpleLayout.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import AppLogoIcon from '@/components/AppLogoIcon.vue';
 import { Link } from '@inertiajs/vue3';
 
 defineProps<{
@@ -14,8 +13,8 @@ defineProps<{
             <div class="flex flex-col gap-8">
                 <div class="flex flex-col items-center gap-4">
                     <Link :href="route('home')" class="flex flex-col items-center gap-2 font-medium">
-                        <div class="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
-                            <AppLogoIcon class="size-9 fill-current text-[var(--foreground)] dark:text-white" />
+                        <div class="mb-1 flex h-36 items-center justify-center rounded-md">
+                            <img src="images/logo-qluub.png" alt="Logo QLuub" class="w-full h-full object-contain" >
                         </div>
                         <span class="sr-only">{{ title }}</span>
                     </Link>
